226ccfb196b32dfc5302100ff769ecea09f4b087
Jessica McLaurin <mclaurinjessica3@gmail.com>
2024-07-09 09:33:58 -0500

    review 1 updates

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,4 +1,4 @@
-import "./WeatherCard.css";
+/* import "./WeatherCard.css";
 import clear from "../../assets/day/clear_day.png";
 import { weatherOptions } from "../../utils/constants";
 import { defaultWeatherOptions } from "../../utils/constants";
@@ -19,6 +19,40 @@ function WeatherCard({ weatherData }) {
       <img
         src={weatherOption?.url}
         alt={weatherOption?.condition}
+        className="weather-card__image"
+      />
+    </section>
+  );
+}
+
+export default WeatherCard; */
+
+import "./WeatherCard.css";
+import clear from "../../assets/day/clear_day.png";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
+
+function WeatherCard({ weatherData }) {
+  const weatherOption = weatherOptions.find((item) => {
+    return (
+      item.day === weatherData.isDay && item.condition === weatherData.condition
+    );
+  });
+
+  const imgUrl =
+    weatherOption?.url ||
+    (weatherData.isDay
+      ? defaultWeatherOptions.day.url
+      : defaultWeatherOptions.night.url);
+
+  console.log(weatherData.isDay); // Debugging
+  console.log(weatherData.condition); // Debugging
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F}° F</p>
+      <img
+        src={imgUrl}
+        alt={weatherOption?.condition || "default"}
         className="weather-card__image"
       />
     </section>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const defaultClothingItems = [
   {
     _id: 0,
     name: "Cap",
-    weather: "Hot",
+    weather: "hot",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
   },
   {
